ec99ebc5a3302890857eb41978ebc26172ec88b9
Eoghan O'Neill <eoneill@palantirsolutions.com>
2018-01-31 15:01:50 +0000

    refactor modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,40 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { StarComponent } from './shared/star.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { ProductDetailComponent } from './products/product-detail.component';
+import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductGuardService } from './products/product-guard.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    StarComponent,
-    ConvertToSpacesPipe,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id',
-        canActivate: [ ProductGuardService ],
-        component: ProductDetailComponent },
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    HttpClientModule,    
+    ProductModule,
+    AppRoutingModule
   ],
-  providers: [ProductGuardService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
